aeae7addc0c82c0433f0f008d1a58d5004f30b95
Eduard Jacko <jacko.eduard@gmail.com>
2022-01-03 16:25:57 +0000

    remvod axios

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,4 +1,3 @@
-import axios, { AxiosInstance } from "axios";
 import {
   serializeOrganisation,
   serializePatient,
@@ -7,40 +6,93 @@ import {
 } from "./serializers";
 import { RawEndpointData } from "../types/RawEndpointData";
 import { Tokens } from "src/types";
-import { fetchJson, httpClient } from "./fetchUtils";
 import { stringify } from "query-string";
 
+type Params = Record<string, string | number | boolean>;
+
+type Respond<T> = Promise<{
+  data: T;
+  headers: Headers;
+}>;
+
+class Fetch {
+  constructor(private apiEndpoint: string, private accessToken: string | null) {
+    // return fetch(
+    //   `https://staging.api.vinehealth.ai/api/v1/pro-dashboard/projects/${programId}/reports/medication?${queryString}`,
+    //   {
+    //     method: "GET",
+    //     headers: new Headers({
+    //       Accept: "application/json",
+    //       Authorization: `Bearer ${this.tokens?.accessToken}`,
+    //     }),
+    //   }
+  }
+
+  post<T>(url: string, data?: any) {
+    return Fetch.post<T>(url, data, { token: this.accessToken || "" });
+  }
+
+  get<T>(url: string, options?: { params?: Params }) {
+    return Fetch.get<T>(url, { token: this.accessToken || "", ...options });
+  }
+
+  static async post<T>(
+    url: string,
+    data?: any,
+    options?: { token?: string }
+  ): Respond<T> {
+    const headers = new Headers({
+      "Content-Type": "application/json",
+    });
+    if (options?.token) headers.set("Authorization", `Bearer ${options.token}`);
+    return fetch(url, {
+      method: "POST",
+      referrerPolicy: "no-referrer",
+      headers,
+      body: JSON.stringify(data || {}),
+    }).then(async (res) => ({
+      data: await res.json(),
+      headers: res.headers,
+    }));
+  }
+  static async get<T>(
+    url: string,
+    options?: { token?: string; params?: Params }
+  ): Respond<T> {
+    const headers = new Headers({
+      "Content-Type": "application/json",
+    });
+    if (options?.token) headers.set("Authorization", `Bearer ${options.token}`);
+    const queryData = options?.params ? stringify(options?.params) : "";
+    const query = queryData ? `?${queryData}` : "";
+    return fetch(`${url}${query}`, {
+      method: "GET",
+      referrerPolicy: "no-referrer",
+      headers,
+    }).then(async (res) => ({
+      data: await res.json(),
+      headers: res.headers,
+    }));
+  }
+}
+
 export class Api {
-  private fetch: AxiosInstance;
+  private fetch: Fetch;
   private tokens: Tokens | null = null;
   constructor(
     private apiEndpoint: string,
     private onTokensUpdate: (tokens: Tokens) => void
   ) {
-    this.fetch = Api.createAxios(apiEndpoint, null);
+    this.fetch = new Fetch(apiEndpoint, null);
   }
   setTokens(tokens: Tokens) {
     this.tokens = tokens;
-    this.fetch = Api.createAxios(this.apiEndpoint, tokens.accessToken);
+    console.log(tokens);
+    this.fetch = new Fetch(this.apiEndpoint, tokens.accessToken);
     this.onTokensUpdate(this.tokens);
   }
-  private static createAxios(apiEndpoint: string, accessToken: string | null) {
-    return axios.create({
-      baseURL: apiEndpoint,
-      withCredentials: true,
-      headers: {
-        // "Content-Type": "application/json",
-        // Accept: "*/*",
-        // "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
-        "Referrer-Policy": "origin",
-        ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
-      },
-    });
-  }
   async login(creds: { email: string; password: string }) {
-    const payload = await axios.post<RawEndpointData["/auth/login"]>(
+    const payload = await this.fetch.post<RawEndpointData["/auth/login"]>(
       `${this.apiEndpoint}/auth/login`,
       creds
     );
@@ -52,16 +104,17 @@ export class Api {
     return {
       payload,
       data: {
-        ...payload.data,
+        ...payload,
         user: serializeUser(payload.data.user),
       },
     };
   }
   async getUser() {
-    const payload = await axios.get<RawEndpointData["/current"]>(
-      `${this.apiEndpoint}/current`,
-      { headers: { Authorization: `Bearer ${this.tokens?.accessToken}` } }
+    // Question this should have Application/json
+    const payload = await this.fetch.get<RawEndpointData["/current"]>(
+      `${this.apiEndpoint}/current`
     );
+    console.log(this);
     return {
       payload,
       data: serializeUser(payload.data),
@@ -88,7 +141,7 @@ export class Api {
       }
     );
     const organisations = payload.data.map(serializeOrganisation);
-    const totalCount = parseInt(payload.headers["x-total-count"]);
+    const totalCount = parseInt(payload.headers.get("x-total-count") || "");
     return {
       organisations,
       totalCount,
@@ -122,7 +175,7 @@ export class Api {
         },
       }
     );
-    const totalCount = parseInt(payload.headers["x-total-count"]);
+    const totalCount = parseInt(payload.headers.get("x-total-count") || "");
     const programs = payload.data.map(serializeProgram);
     return {
       totalCount,
@@ -136,10 +189,9 @@ export class Api {
     return { payload, data: serializeProgram(payload.data) };
   }
   async refetchAccessToken() {
-    const payload = await axios.post<RawEndpointData["/auth/refresh"]>(
-      `${this.apiEndpoint}/auth/refresh`,
-      {},
-      { headers: { Authorization: `Bearer ${this.tokens?.refreshToken}` } }
+    // Question this should have Application/json
+    const payload = await this.fetch.post<RawEndpointData["/auth/refresh"]>(
+      `${this.apiEndpoint}/auth/refresh`
     );
     this.setTokens({
       accessToken: payload.data.accessToken,
@@ -172,7 +224,7 @@ export class Api {
         consentedOnly,
       },
     });
-    const totalCount = parseInt(payload.headers["x-total-count"]);
+    const totalCount = parseInt(payload.headers.get("x-total-count") || "");
     return {
       totalCount,
       patients: payload.data.map(serializePatient),
@@ -190,24 +242,6 @@ export class Api {
     >(`admin/projects/${programId}/users/${participantId}`);
     return { payload, data: serializePatient(payload.data) };
   }
-
-  // async getMedications({
-  //   programId,
-  //   startDate,
-  //   endDate,
-  // }: {
-  //   programId: string;
-  //   startDate: Date;
-  //   endDate: Date;
-  // }) {
-  //   const payload = await fetch.get<
-  //     RawEndpointData["/pro-dashboard/projects/:projectId/reports/medication"]
-  //   >(`/pro-dashboard/${programId}/reports/medication`, {
-  //     params: {
-  //       startDate: startDate.toISOString(),
-  //       endDate: endDate.toISOString(),
-  //     },
-  //   });
   async getMedications({
     programId,
     startDate,
@@ -221,38 +255,15 @@ export class Api {
       startDate: startDate.toISOString(),
       endDate: endDate.toISOString(),
     });
-    return httpClient<{}>(
+    return fetch(
       `https://staging.api.vinehealth.ai/api/v1/pro-dashboard/projects/${programId}/reports/medication?${queryString}`,
       {
         method: "GET",
         headers: new Headers({
           Accept: "application/json",
+          Authorization: `Bearer ${this.tokens?.accessToken}`,
         }),
-      },
-      this.tokens?.accessToken
-    );
-    // const x = await httpClient(
-    //   `https://staging.api.vinehealth.ai/api/v1/pro-dashboard/${programId}/reports/medication?startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}`,
-    //   {},
-    //   this.tokens?.accessToken
-    // );
-    // const payload = await window.fetch(
-    //   `https://staging.api.vinehealth.ai/api/v1/pro-dashboard/${programId}/reports/medication?startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}`,
-    //   {
-    //     headers: new Headers({
-    //       // "Content-Type": "application/json",
-    //       // Accept: "*/*",
-    //       // "Access-Control-Allow-Origin": "*",
-    //       "Access-Control-Allow-Origin": "*",
-    //       "Content-Type": "application/json",
-    //       "Referrer-Policy": "origin",
-    //       Authorization: `Bearer ${this.tokens?.accessToken}`,
-    //     }),
-    //   }
-    // );
-    // const data = await payload.json();
-    // console.log(data);
-    // return payload;
-    return {};
+      }
+    ).then((v) => v.json());
   }
 }
